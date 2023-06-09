00faea83c264ac2e3b653de5d21719e9cb89254a
CarlosPozoC <91538573+CarlosPozoC@users.noreply.github.com>
2023-06-10 00:53:34 +0200

    Creacion del componente register
    Creacion de un componente register que se encarga de que la contraseña tenga entre 8-20 caracteres, que tenga una mayuscula, minuscula y numero, que el sexo sea masculino o femenino y que la contraseña este encriptada en el envio a la base de datos mediante el metodo post de la api

--- GastroLab/src/app/app.module.ts
+++ GastroLab/src/app/app.module.ts
@@ -4,18 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 // Importa otros componentes si es necesario
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestpageComponent
+    TestpageComponent,
+    RegisterComponent,
     // Agrega otros componentes aquí si es necesario
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule // Asegúrate de que AppRoutingModule esté importado aquí
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule // Asegúrate de que AppRoutingModule esté importado aquí
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ GastroLab/src/app/register/register.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { NgForm, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { hashSync } from 'bcryptjs';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  contrasenaControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.maxLength(20),
+    Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,20}$')
+  ]);
+
+  contrasenaValida = false;
+
+  constructor(private http: HttpClient) {}
+
+  registrarUsuario(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+
+    const contraseñaEncriptada = hashSync(form.value.contrasena, 10);
+
+    const usuario = {
+      nombre: form.value.nombre,
+      contrasena: contraseñaEncriptada,
+      sexo: form.value.sexo
+    };
+
+    this.http.post('https://localhost:7271/api/Usuario', usuario).subscribe(
+      response => {
+        console.log(response);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  validarContrasena() {
+    this.contrasenaValida = this.contrasenaControl.valid;
+  }
+}
