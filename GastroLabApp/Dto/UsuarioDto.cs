59a073fca11d93accb7b21bb96f2b75e44a256f2
CarlosPozoC <91538573+CarlosPozoC@users.noreply.github.com>
2023-06-10 00:55:59 +0200

    Cambios por letra ñ
    Cambiado el cambio de contraseña a contrasena para la correcta insercion en base de datos

--- GastroLabApp/Dto/UsuarioDto.cs
+++ GastroLabApp/Dto/UsuarioDto.cs
@@ -6,7 +6,7 @@ namespace GastroLabApp.Dto
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
-        public string Contraseña { get; set; }
+        public string Contrasena { get; set; }
         public string Sexo { get; set; }
     }
 }
--- GastroLabApp/Models/Usuario.cs
+++ GastroLabApp/Models/Usuario.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
-        public string Contraseña { get; set; }
+        public string Contrasena { get; set; }
         public string Sexo { get; set; }
         public ICollection<Receta>? Recetas { get; set; }
         public ICollection<Opinion>? Opiniones { get; set; }
